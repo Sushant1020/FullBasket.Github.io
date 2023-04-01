7f810ba192e08689aa2c6d862f68e805e457a85e
Sushant1020 <113348541+Sushant1020@users.noreply.github.com>
2023-04-01 18:59:37 +0530

    FullBasket

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const path = require("path");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/users");
+const methodOverride = require("method-override");
+const mongoSanitize = require("express-mongo-sanitize");
+const ExpressError = require("./utils/ExpressError");
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const path = require('path');
-const flash = require('connect-flash');
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/users');
-const methodOverride = require('method-override');
-const mongoSanitize = require('express-mongo-sanitize');
-const ExpressError = require('./utils/ExpressError');
-require('dotenv').config();
-
-const url = process.env.DB_URL
+const url = process.env.DB_URL;
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,74 +25,55 @@ db.once("open", () => {
 });
 const app = express();
 
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 
-
-
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(flash());
-app.use(mongoSanitize())
-
-
-
-
-
+app.use(mongoSanitize());
 
 const sessionConfig = {
-  secret: 'thisshouldbeabettersecret!',
+  secret: "thisshouldbeabettersecret!",
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 1000 * 60 * 60 * 24 * 7
-  }
-}
-app.use(session(sessionConfig))
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
 app.use(flash());
 
-
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()))
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-const productRoutes = require('./routes/products');
-const userRoutes = require('./routes/users');
+const productRoutes = require("./routes/products");
+const userRoutes = require("./routes/users");
 
 app.use(flash());
-app.use('/products', productRoutes)
-app.use('/', userRoutes)
-
-
+app.use("/products", productRoutes);
+app.use("/", userRoutes);
 
 app.use((req, res, next) => {
   console.log(req.query);
   res.locals.currentUser = req.user;
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
   next();
-})
+});
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-
-
-
-const port = process.env.PORT
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Serving on port 3000');
-})
+app.listen(3000, () => {
+  console.log("Serving on port 3000");
+});
--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,42 @@
-const users = require('../controllers/users');
-const isLoggedIn = require('../middleware');
-const ExpressError = require('../utils/ExpressError');
-const express = require('express');
-const catchAsync = require('../utils/catchAsync');
-const router = require('express').Router();
-const passport = require('passport');
+const users = require("../controllers/users");
+const product = require("../controllers/product");
+const isLoggedIn = require("../middleware");
+const ExpressError = require("../utils/ExpressError");
+const express = require("express");
+const catchAsync = require("../utils/catchAsync");
+const router = require("express").Router();
+const passport = require("passport");
+const products = require("../models/products");
 
-router.route('/register')
-  .get((users.register))
+router.route("/").get(product.home);
+
+router
+  .route("/register")
+  .get(users.register)
   .post(catchAsync(users.registeruser));
 
-
-router.route('/login')
+router
+  .route("/login")
   .get(users.renderLogin)
-  .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    users.login
+  );
 
 // router.route('/cart')
 //   .get(users.rendercart);
 
-router.route('/cart/:id')
-  .get(users.cartshowadded);
+router.route("/cart/:id").get(users.cartshowadded);
 
-router.route('/cart')
-  .get(users.showusercart);
+router.route("/cart").get(users.showusercart);
 
-router.route('/cartremove/:id')
-  .get(users.cartremove);
+router.route("/cartremove/:id").get(users.cartremove);
 
-router.get('/logout', users.logout);
+router.get("/logout", users.logout);
 
-router.route('/viewprofile')
-  .get(users.viewprofile);
-
+router.route("/viewprofile").get(users.viewprofile);
 
 module.exports = router;
